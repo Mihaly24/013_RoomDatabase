49aeac87f25096ab031258fcae0edefb3e41b850
Mihaly24 <fairuzk20@gmail.com>
2025-11-28 15:24:36 +0700

    Menambah fungsi insert pada SiswaDao.kt

--- app/src/main/java/com/example/roomdb/room/SiswaDao.kt
+++ app/src/main/java/com/example/roomdb/room/SiswaDao.kt
@@ -11,4 +11,8 @@ import kotlinx.coroutines.flow.Flow
 interface SiswaDao {
     @Query("SELECT * FROM tblSiswa ORDER BY nama ASC")
     fun getAllSiswa(): Flow<List<Siswa>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa
+    )
 }
