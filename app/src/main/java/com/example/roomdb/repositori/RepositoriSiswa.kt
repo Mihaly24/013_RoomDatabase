6d46857fc388ddec921e1834e6aef7a54377e22b
Mihaly24 <fairuzk20@gmail.com>
2025-11-28 15:40:55 +0700

    Membuat interface RepositoriSiswa

--- app/src/main/java/com/example/roomdb/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/roomdb/repositori/RepositoriSiswa.kt
@@ -1,2 +1,10 @@
 package com.example.roomdb.repositori
 
+import com.example.roomdb.room.Siswa
+import com.example.roomdb.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa{
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+}
