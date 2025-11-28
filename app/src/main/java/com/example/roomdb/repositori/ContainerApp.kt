7be6f72bd262daa66aecbd15904e4aa30a1e50fc
Mihaly24 <fairuzk20@gmail.com>
2025-11-28 15:55:26 +0700

    Membuat interface ContainerApp

--- app/src/main/java/com/example/roomdb/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/roomdb/repositori/ContainerApp.kt
@@ -1,2 +1,9 @@
 package com.example.roomdb.repositori
 
+import android.app.Application
+import android.content.Context
+import com.example.roomdb.room.DatabaseSiswa
+
+interface ContainerApp{
+    val repositoriSiswa : RepositoriSiswa
+}
