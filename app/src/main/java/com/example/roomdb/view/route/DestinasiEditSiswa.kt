8e110962d2bc501fdc1a830a456280ff820c6b10
Mihaly24 <fairuzk20@gmail.com>
2025-12-05 18:13:56 +0700

    Menambah file DestinasiEditSiswa.kt

---
+++ app/src/main/java/com/example/roomdb/view/route/DestinasiEditSiswa.kt
@@ -0,0 +1,10 @@
+package com.example.roomdb.view.route
+
+import com.example.roomdb.R
+
+object DestinasiEditSiswa : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "itemId"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
