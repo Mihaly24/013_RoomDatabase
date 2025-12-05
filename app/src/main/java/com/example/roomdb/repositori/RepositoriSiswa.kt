dbaf854ccd09eac146805e435609f9eb0f36c619
Mihaly24 <fairuzk20@gmail.com>
2025-12-05 14:57:12 +0700

    Menambah override update

--- app/src/main/java/com/example/roomdb/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/roomdb/repositori/RepositoriSiswa.kt
@@ -20,5 +20,6 @@ class OfflineRepositoriSiswa(
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
     //edit 2: nambah 3 override
     override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
-    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
 }
